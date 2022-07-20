0dc3e7b74a578d476d87ec501309b8f7b4c424ac
Muhammad <106791645+Muhammad-me@users.noreply.github.com>
2022-07-20 21:54:28 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const input = document.querySelector("#input");
+const avatar = document.querySelector("#avatar");
+
+function Avatar(){
+    const url = `https://avatars.dicebear.com/api/avataaars/${input.value}.svg`;
+    avatar.src = url;
+}
+input.addEventListener("keyup", Avatar)
